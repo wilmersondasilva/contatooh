b07d7632de5b5c9cbfa9c8425d2f73026c3c0107
Wilmerson da Silva <wilmersondasilva@gmail.com>
2016-09-15 20:38:30 -0300

    Unit and e2e tests added

--- config/protractor.js
+++ config/protractor.js
@@ -6,8 +6,8 @@ exports.config = {
 function onPrepare() {
 	browser.driver.get('http://localhost:3000').then(function() {
 		browser.driver.findElement(by.id('enter')).click();
-		browser.driver.findElement(by.id('login_field')).sendKeys('wilmersondasilva@gmail.com');
-		browser.driver.findElement(by.id('password')).sendKeys('aquiles2');
+		browser.driver.findElement(by.id('login_field')).sendKeys('');
+		browser.driver.findElement(by.id('password')).sendKeys('');
 		browser.driver.findElement(by.name('commit')).click();
 	});
 }
